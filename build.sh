a4e525d228948ec50abd5fb84bb07af85262b72a
Nathan Chancellor <natechancellor@gmail.com>
2017-01-23 14:19:30 +0100

    Use ccache to compile

--- build.sh
+++ build.sh
@@ -1,4 +1,52 @@
+export USE_CCACHE=1
+export CCACHE_DIR=$PWD/.ccache
+if [ ! -d $PWD/.ccache ]; then
+ccache -M 1G
+fi
 export ARCH=arm64
+
+function get_make_command()
+{
+  echo command make
+}
+function make()
+{
+    local start_time=$(date +"%s")
+    $(get_make_command) "$@"
+    local ret=$?
+    local end_time=$(date +"%s")
+    local tdiff=$(($end_time-$start_time))
+    local hours=$(($tdiff / 3600 ))
+    local mins=$((($tdiff % 3600) / 60))
+    local secs=$(($tdiff % 60))
+    local ncolors=$(tput colors 2>/dev/null)
+    if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
+        color_failed=$'\E'"[0;31m"
+        color_success=$'\E'"[0;32m"
+        color_reset=$'\E'"[00m"
+    else
+        color_failed=""
+        color_success=""
+        color_reset=""
+    fi
+    echo
+    if [ $ret -eq 0 ] ; then
+        echo -n "${color_success}#### make completed successfully "
+    else
+        echo -n "${color_failed}#### make failed to build some targets "
+    fi
+    if [ $hours -gt 0 ] ; then
+        printf "(%02g:%02g:%02g (hh:mm:ss))" $hours $mins $secs
+    elif [ $mins -gt 0 ] ; then
+        printf "(%02g:%02g (mm:ss))" $mins $secs
+    elif [ $secs -gt 0 ] ; then
+        printf "(%s seconds)" $secs
+    fi
+    echo " ####${color_reset}"
+    echo
+    return $ret
+}
+
 
 if [ -d ~/toolchain/aarch64-4.9/bin/ ]; then
 	echo "Found toolchain: Setting up . . . "
