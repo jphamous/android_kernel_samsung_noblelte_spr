b962572523b889df9677ba60bb2a5e0c96d4f236
Snuzzo <jrracinfan@gmail.com>
2017-05-22 03:38:16 -0400

    Build script: Add # of cores & build section

--- build.sh
+++ build.sh
@@ -9,3 +9,33 @@ fi
 export CROSS_COMPILE=~/toolchain/aarch64-4.9/bin/aarch64-linux-android-
 
 make exynos7420-noblelte_usa_spr_defconfig
+
+while read -p "Start build with:`echo $'\n ' `1) $(nproc) cores?`echo $'\n ' `2) $(($(nproc)/2)) cores?`echo $'\n '`3) $(($(nproc)/4)) cores?`echo $'\n '`X|x to cancel auto build`echo $'\n>'`" corechoice
+do
+case "$corechoice" in
+	1 )	
+		echo "Building with $(nproc) cores"
+		make -j$(nproc)
+		break
+		;;
+
+        2 )
+		echo "Building with $(($(nproc)/2)) cores"
+		make -j$(($(nproc)/2))
+                break
+                ;;
+        3 )
+		echo "Building with $(($(nproc)/4)) core(s)"
+		make -j$(($(nproc)/4))
+		break
+                ;;
+        X|x )
+                echo "Cancelling auto build"
+                break
+                ;;
+        * )
+                echo "Invalid: Try again"
+                ;;
+
+esac
+done
